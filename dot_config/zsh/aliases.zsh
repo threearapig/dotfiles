680a3068aa92ae85200d06e22aa425cd79ebf24e
TheJC <threearapig@gmail.com>
2023-09-01 01:18:13 +0800

    add zsh

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,2 @@
+alias ra='joshuto'
+alias che='chezmoi'
---
+++ dot_config/zsh/env.zsh
@@ -0,0 +1,5 @@
+export EDITOR=nvim
+export XDG_CONFIG_HOME=$HOME/.config
+export ZIM_CONFIG_FILE=~/.config/zsh/zimrc
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
