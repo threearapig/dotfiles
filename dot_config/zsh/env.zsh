ca8d7adee980881f1b8bad70a2d2aac02dc21d9c
TheJC <threearapig@gmail.com>
2023-10-03 14:55:48 +0800

    Zsh: some changes

--- dot_config/zsh/env.zsh
+++ dot_config/zsh/env.zsh
@@ -3,3 +3,4 @@ export XDG_CONFIG_HOME=$HOME/.config
 export ZIM_CONFIG_FILE=~/.config/zsh/zimrc
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
 export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+export ALIYUNPAN_CONFIG_DIR=$HOME/.config/aliyunpan/config
