c31a86c21a704c2bbc1d5f1e14608a4b18f0207c
TheJC <threearapig@gmail.com>
2023-10-27 16:31:27 +0800

    some changes

--- dot_config/nvim/lua/config/plugins/ui/colorscheme.lua
+++ dot_config/nvim/lua/config/plugins/ui/colorscheme.lua
@@ -1,8 +1,14 @@
 return {
-	"theniceboy/nvim-deus",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd[[colorscheme deus]]
-	end,
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+	config = function ()
+		vim.cmd[[colorscheme tokyonight]]
+	end
+	-- "theniceboy/nvim-deus",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function ()
+	-- 	vim.cmd[[colorscheme deus]]
+	-- end
 }
